a36b88dede2a05f7e3c83f6359f6ee6a5a77b5ad
Marina Standriichuk <standriichuk.m@gmail.com>
2021-08-07 22:38:58 +0300

    Add the solution to the task

--- index.js
+++ index.js
@@ -6,3 +6,37 @@ const colors = [
   '#009688',
   '#795548',
 ];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const refs = {
+  startBtn: document.querySelector('button[data-action="start"]'),
+  stopBtn: document.querySelector('button[data-action="stop"]'),
+};
+
+refs.stopBtn.disabled = true;
+
+const bgColorSwitch = {
+  start() {
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+    this.isActive = true;
+    this.timerId = setInterval(() => {
+      document.body.style.backgroundColor =
+        colors[randomIntegerFromInterval(0, colors.length - 1)];
+    }, 1000);
+  },
+  stop() {
+    refs.stopBtn.disabled = true;
+    refs.startBtn.disabled = false;
+    clearInterval(this.timerId);
+  },
+};
+
+refs.startBtn.addEventListener(
+  'click',
+  bgColorSwitch.start.bind(bgColorSwitch),
+);
+refs.stopBtn.addEventListener('click', bgColorSwitch.stop.bind(bgColorSwitch));
